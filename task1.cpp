c4d5e5887f8de7bbde8473551cbb219e46fab9ff
Wahida-shahi <141648670+Wahida-shahi@users.noreply.github.com>
2023-08-20 23:23:39 +0530

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -1,99 +1,93 @@
-/*Create a program that generates a random number and asks the
-user to guess it. Provide feedback on whether the guess is too
-high or too low until the user guesses the correct number.*/
-/*NUMBER GUESSING GAME*//*DONE*/
+/*Develop a calculator program that performs basic arithmetic
+operations such as addition, subtraction, multiplication, and
+division. Allow the user to input two numbers and choose an
+operation to perform.*//*DONE*/
+/*SIMPLE CALCULATOR*/
 #include<iostream>
-#include <cstdlib>
-#include<ctime>
+#include<math.h>
 using namespace std;
-int guess(int level,int userguess)
+int add(int num1,int num2)
 {
-    while (true)
+    return num1+num2;
+}
+
+int sub(int num1,int num2)
+{
+    return num1-num2;
+}
+int mul(int num1,int num2)
+{
+    return num1*num2;
+}
+int divid(int num1,int num2)
+{
+    return num1/num2;
+}
+int option()
+{
+    cout<<"\nEnter the option you want to be perform\n";
+        cout<<"**************************************\n";
+        cout<<"**1= ADD +\n";
+        cout<<"**2= sub -\n";
+        cout<<"**3= multiple *\n";
+        cout<<"**4= divide /\n";
+        cout<<"**************************************\n";
+        int choice;
+        cin>>choice;
+        cout<<"Your choice is "<<choice<<endl;
+    return choice;
+ } 
+ int clc(int choice, int num1, int num2)
+{
+    int result;
+    switch(choice)
     {
-        cout << "\nDifficulty Levels\n";
-        cout << "\n1 for starte the game!\t";
-        cout << "\n0 for ending the game!\n" << endl;
-        //int level;
-        cout << "Enter the difficulty level number: ";
-        cin >> level;
-        srand(time(0));
-        int size=100;
-        int rightAnswer = (rand() % (size-1)) + 1;
-        //int userguess;
-        if(level==1)
+        case 1:
+        cout<<"\nThe addition of two numbers is ="; 
+        result=add(num1,num2);
+        break;
+
+        case 2:
+        cout<<"\nThe subtraction of two numbers is=";
+        result=sub(num1,num2);
+        break;
+
+        case 3:
+        cout<<"\nThe multiplication of two numbers is=";
+        result=mul(num1,num2);
+        break;
+
+        case 4:
+        if(num2 == 0)
         {
-            cout<<"You will get 3 chance to guess the right anser\n";
-            int chance=3;
-            for(int i=0;i<3;i++)
-                {    
-                cout<<"\nNow let guess the number:\n";
-                cin>>userguess;
-                if(userguess==rightAnswer)
-                {
-                
-                cout<<"the( "<<userguess<<" )Yuor answer is correct \n";
-                break;
-                }
-                else if(userguess>rightAnswer)
-                    {
-                        cout<<"The( "<< userguess <<" )Your answer is too high\n"<<endl;
-                    }
-                else{
-                    
-                        cout<<"The( "<< userguess <<" )Your answer is too low\n"<<endl;
-                }
-                chance--;
-                if(chance==0)
-                {
-                    cout<<"the rondom number is = "<<rightAnswer<<endl;
-                }
-            }
-        }
-        // To end the game
-        else if (level == 0) 
-        {
-            exit(0);
+            cout<<"Error !! divition by zero\n";
+            exit(1);
         }
         else
-         {
-            cout << "Kindly select between (0,1)"<< endl;
-        }
-    }    
- }
+        cout<<"\nThe division of two numbers is=";
+        result=divid(num1,num2);
+        break;
+        default:
+        cout<<"\nOperation is not avaliable\n";
+        exit(1);
+    }
+    return result;
+}
+ 
 int main()
 {
-    cout<<"\nWELCOME TO THE NUMBER GUESSING GAME <0_0>\n";
-    cout<<"Here the user need to guess the number which is randomly generates by computer and the user will guess\n ";
-    cout<<"\nGOOD LUCK MY FRIND\n";
-    int size=100;
-    int hight=size;
-    int low=1;
-    //for generate different random numbers each time the game is played
-     srand(time(0));
-     int rightAnswer = rand() % size + 1;
-    int chance=0;
-    int  userguess;
-    int level;
-    int g=guess(level,userguess);
-    cout<<g;
-    do
-    {   
-         cout<<"\nNow let guess the number:\n";
-          cin>>userguess;
-          chance++;
-         if(userguess==rightAnswer)
-         {
-         
-          cout<<"the( "<<userguess<<" )Yuor answer is correct \n";
-           break;
-         }
-        else if(userguess>rightAnswer)
-            {
-                cout<<"The( "<< userguess <<" )Your answer is too high\n"<<endl;
-            }
-          else{
-            cout << "\nGood! You got the right guess "<<userguess <<"in "<< chance << " try\n";    
-          }
-    }while (userguess!=rightAnswer);
+    int num1,num2;
+    cout<<"\nEnter the first number\n";
+    cin>>num1;
+    cout<<"\nEnter the second number\n";
+    cin>>num2;
+    int choice=option();
+    int result=clc(choice,num1,num2 );
+    cout<<result;
     return 0;
 }
+
+
+  
+
+
